77760c1e1650e6945e6dee3c6b10fca5ed5176b8
NikitaFE <nikromongerredd@gmail.com>
2020-07-14 12:20:03 +0300

    Updates

---
+++ precache-manifest.06fa9215c5cc99288279b510446536fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa16acc0f17755923a49f74cead113c",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "431f5c51cfae3ee9f4e5",
+    "url": "/react-movie-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0780a04612414771bad6",
+    "url": "/react-movie-app/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "431f5c51cfae3ee9f4e5",
+    "url": "/react-movie-app/static/js/2.38eb9764.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-movie-app/static/js/2.38eb9764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0780a04612414771bad6",
+    "url": "/react-movie-app/static/js/main.2902c472.chunk.js"
+  },
+  {
+    "revision": "485c664acc0a22b8e01d",
+    "url": "/react-movie-app/static/js/runtime-main.50d24105.js"
+  }
+]);
